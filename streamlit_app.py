f70421b9b36cf0317ce009aee62714734379b9c4
Jimmy Calvo Monge <80930754+JimmyCalvoMonge@users.noreply.github.com>
2024-03-06 14:29:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session = cnx.session()
 
 my_dataframe = session.table(
     "smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-pd_dataframe = my_dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 
 
 ingredients_list = st.multiselect(
